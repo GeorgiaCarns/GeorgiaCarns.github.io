f9f38b1ce6991a2264344d913e304cb18065951a
Georgia Carns <georgiacarns@gmail.com>
2019-11-01 09:45:42 +1100

    rename

---
+++ dpub/geo-graphics/assets/js/main.js
@@ -0,0 +1,14 @@
+$( document ).ready(function() {
+    
+
+setTimeout(function(){
+    $('.tap1').addClass('active')
+},2300);
+
+setTimeout(function(){
+    $('.tap2').addClass('active')
+},1600);
+
+setTimeout(function(){
+    $('.tap3').addClass('active')
+},1900);
